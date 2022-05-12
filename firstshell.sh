9d4b37b547c7fd6cb306f65fa1e99456cbd4efcb
shreegowri <gaganandh@gmail.com>
2022-05-12 14:52:31 +0530

    shreegowri elif commands

--- firstshell.sh
+++ firstshell.sh
@@ -1,9 +1,47 @@
-#! /bin/bash/ -x
+#! /bin/bash -x
 
-echo "welcome to shell"
-read city
-#read -p "enter name" fullname
-echo "enter city name"
-#fullname="gagana"
-echo $fullname
-echo $city
+condition='false'
+trueCondition='value'
+echo "*****************************************"
+if [ $condition == 'item' ]
+then
+        echo "this is item"
+fi
+echo "*****************************************"
+if [ $condition == 'item' ]
+then
+        echo "this is item"
+else
+        echo "this is not item"
+fi
+echo "*****************************************"
+if [ $condition == 'item' ]
+then
+        echo "this is item"
+elif [ $condition == 'false' ]
+then
+        echo "the condition is false"
+else
+        echo "the conditon is not false"
+fi
+echo "*****************************************"
+
+#nested if
+if [ $condition == 'true' ]
+then
+        echo "Condition is true "
+        if [ $trueCondition == 'value' ]
+        then
+                echo "Condition is value"
+        else
+                echo "Condition is not value"
+        fi
+else
+        echo "Condition is false "
+fi
+
+
+
+
+
+
